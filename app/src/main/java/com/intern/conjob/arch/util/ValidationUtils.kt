8ea74014b8767e82b9d54103283a8e33de6e74cc
rikkei-KhoaND <khoand_tts@rikkeisoft.com>
2024-04-16 16:12:06 +0700

    CJM-73_Fix

--- app/src/main/java/com/intern/conjob/arch/util/ValidationUtils.kt
+++ app/src/main/java/com/intern/conjob/arch/util/ValidationUtils.kt
@@ -7,6 +7,12 @@ fun String.isValidPassword(): Boolean {
     return Pattern.compile("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@\$%^&*-]).{8,32}\$").matcher(this).matches()
 }
 
+// ít nhất 2 kí tự
+// @
+// ít nhất 1 kí tự
+// .
+// từ 2 đến 4 kí tự
+// vd: ab@gmail.com
 fun String.isValidEmail(): Boolean {
     return Pattern.compile("^[A-Za-z][\\w\\-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}\$").matcher(this).matches()
 }
