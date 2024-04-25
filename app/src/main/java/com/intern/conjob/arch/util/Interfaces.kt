153a72f0961c94c5b15e8a3d6bd31bdf02037cb6
rikkei-KhoaND <khoand_tts@rikkeisoft.com>
2024-04-25 09:10:00 +0700

    CJM-154_Add_Matching_Feed_UI

---
+++ app/src/main/java/com/intern/conjob/arch/util/Interfaces.kt
@@ -0,0 +1,9 @@
+package com.intern.conjob.arch.util
+
+interface PostOnClickListener {
+    fun onDetailClick()
+    fun onAvatarClick()
+    fun onInteractClick()
+    fun onCommentClick()
+    fun onShareClick()
+}
