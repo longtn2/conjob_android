f683a1855691ee6dcb88af9710674e446fc038ba
rikkei-KhoaND <khoand_tts@rikkeisoft.com>
2024-04-26 16:07:24 +0700

    CJM-154_Update_Matching_Feed_UI

--- app/src/main/java/com/intern/conjob/ui/home/matching/MatchingFragment.kt
+++ app/src/main/java/com/intern/conjob/ui/home/matching/MatchingFragment.kt
@@ -6,26 +6,27 @@ import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
-import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.view.size
 import androidx.fragment.app.viewModels
 import com.intern.conjob.R
 import com.intern.conjob.arch.extensions.viewBinding
 import com.intern.conjob.arch.util.Constants.BLUR_EFFECT_RADIUS
-import com.intern.conjob.arch.util.Constants.MIN_SWIPE_PROGRESS
 import com.intern.conjob.arch.util.PostOnClickListener
 import com.intern.conjob.databinding.FragmentMatchingBinding
 import com.intern.conjob.ui.MainActivity
 import com.intern.conjob.ui.base.BaseFragment
 import com.intern.conjob.ui.base.BaseViewModel
 import com.intern.conjob.ui.home.matching.adapter.PostAdapter
-import com.yalantis.library.KolodaListener
-import kotlin.math.abs
+import com.yuyakaido.android.cardstackview.CardStackLayoutManager
+import com.yuyakaido.android.cardstackview.CardStackListener
+import com.yuyakaido.android.cardstackview.Direction
 
 
 class MatchingFragment : BaseFragment(R.layout.fragment_matching) {
     private val binding by viewBinding(FragmentMatchingBinding::bind)
     private val viewModel by viewModels<MatchingViewModel>()
     private var adapter: PostAdapter? = null
+    private var cardView: View? = null
 
     companion object {
         fun newInstance() = MatchingFragment()
@@ -66,68 +67,73 @@ class MatchingFragment : BaseFragment(R.layout.fragment_matching) {
                     Toast.makeText(context, getString(R.string.toast_matching_share), Toast.LENGTH_SHORT).show()
                 }
             })
-            koloda.adapter = adapter
-            koloda.kolodaListener = object : KolodaListener {
-                override fun onCardSwipedLeft(position: Int) {
-                    super.onCardSwipedLeft(position)
-                    Toast.makeText(context, getString(R.string.toast_matching_skip), Toast.LENGTH_SHORT).show()
-                    changeStatusVisibility(false)
-                }
 
-                override fun onCardSwipedRight(position: Int) {
-                    super.onCardSwipedRight(position)
-                    Toast.makeText(context, getString(R.string.toast_matching_accept), Toast.LENGTH_SHORT).show()
-                    changeStatusVisibility(false)
-                }
-
-                override fun onEmptyDeck() {
-                    super.onEmptyDeck()
-                    koloda.reloadAdapterData()
-                }
-
-                override fun onCardDrag(position: Int, cardView: View, progress: Float) {
-                    super.onCardDrag(position, cardView, progress)
-                    when {
-                        abs(progress) > MIN_SWIPE_PROGRESS -> {
-                            with(progress > 0) {
-                                cardView.foreground = if (this) {
-                                    AppCompatResources.getDrawable(
-                                        activity as MainActivity,
-                                        R.color.swipe_accept_bg_color
-                                    )
-                                } else {
-                                    AppCompatResources.getDrawable(
-                                        activity as MainActivity,
-                                        R.color.swipe_skip_bg_color
-                                    )
-                                }
-                                changeStatusView(this)
-                            }
-                            changeStatusVisibility(true)
-                            createBlurEffect(cardView)
-                        }
-                        else -> {
-                            cardView.foreground = null
-                            changeStatusVisibility(false)
-                            changeStatusView(false)
-                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-                                cardView.setRenderEffect(null)
-                            }
-                        }
+            val cardLayoutManager = CardStackLayoutManager(activity as MainActivity, object: CardStackListener {
+                override fun onCardDragging(direction: Direction?, ratio: Float) {
+                    if (cardView!!.x < 0) {
+                        changeStatusVisibility(true)
+                        changeStatusView(false)
+                    } else {
+                        changeStatusVisibility(true)
+                        changeStatusView(true)
                     }
-                    constraintLayoutStatus.x = cardView.x
-                    constraintLayoutStatus.y = cardView.y
-                    constraintLayoutStatus.rotation = cardView.rotation
+                    binding.apply {
+                        constraintLayoutStatus.x = cardView!!.x
+                        constraintLayoutStatus.y = cardView!!.y
+                        constraintLayoutStatus.rotation = cardView!!.rotation
+                    }
                 }
-            }
+
+                override fun onCardSwiped(direction: Direction?) {
+                    if (cardView!!.x < 0) {
+                        Toast.makeText(activity as MainActivity, getString(R.string.toast_matching_skip), Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(activity as MainActivity, getString(R.string.toast_matching_accept), Toast.LENGTH_SHORT).show()
+                    }
+                    if (cardStackView.size <= 0) {
+                        adapter?.posts = viewModel.getTempData()
+                    }
+                }
+
+                override fun onCardRewound() = Unit
+
+                override fun onCardCanceled() {
+                    changeStatusVisibility(false)
+                }
+
+                override fun onCardAppeared(view: View?, position: Int) {
+                    cardView = view
+                }
+
+                override fun onCardDisappeared(view: View?, position: Int) {
+                    changeStatusVisibility(false)
+                }
+
+            })
+            cardLayoutManager.setVisibleCount(3)
+            cardLayoutManager.setMaxDegree(45f)
+            cardLayoutManager.setSwipeThreshold(0.3f)
+            cardLayoutManager.setDirections(Direction.FREEDOM)
+            cardStackView.layoutManager = cardLayoutManager
+            cardStackView.adapter = adapter
         }
     }
 
     private fun changeStatusVisibility(isShow: Boolean) {
         if (isShow) {
             binding.constraintLayoutStatus.visibility = View.VISIBLE
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                cardView!!.setRenderEffect(
+                    RenderEffect.createBlurEffect(
+                        BLUR_EFFECT_RADIUS, BLUR_EFFECT_RADIUS, Shader.TileMode.MIRROR
+                    )
+                )
+            }
         } else {
             binding.constraintLayoutStatus.visibility = View.GONE
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                cardView!!.setRenderEffect(null)
+            }
         }
     }
 
@@ -138,16 +144,6 @@ class MatchingFragment : BaseFragment(R.layout.fragment_matching) {
         } else {
             binding.imgStatus.setImageResource(R.drawable.ic_matching_skip)
             binding.tvStatus.text = getString(R.string.matching_skip)
-        }
-    }
-
-    private fun createBlurEffect(cardView: View) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            cardView.setRenderEffect(
-                RenderEffect.createBlurEffect(
-                    BLUR_EFFECT_RADIUS, BLUR_EFFECT_RADIUS, Shader.TileMode.MIRROR
-                )
-            )
         }
     }
 }
